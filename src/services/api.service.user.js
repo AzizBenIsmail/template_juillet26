4d772f22f8b069f689ae86f6cbb7d3310611b550
AzizBenIsmail <aziz.270700@gmail.com>
2026-07-18 10:42:00 +0100

    Update api.service.user.js

--- src/services/api.service.user.js
+++ src/services/api.service.user.js
@@ -5,3 +5,7 @@ const apiUrl = 'http://localhost:5000/users/'
 export async function getUsers() {
     return await axios.get(`${apiUrl}/getAllUsers`);
 }
+
+export async function deleteUser(userId) {
+    return await axios.delete(`${apiUrl}/deleteUser/${userId}`);
+}
